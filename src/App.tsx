12ed02b5fad72db8ce52c05ffaacf8f624fa65b1
nagyb3 <nagybence2003@gmail.com>
2023-06-04 22:28:13 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAnalytics } from "firebase/analytics";
+import {getFirestore, collection, getDocs} from 'firebase/firestore/lite'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBt_EqN0Oq85nzt7zfmdXbOG6cye9xgNrA",
+  authDomain: "social-media-clone-de887.firebaseapp.com",
+  projectId: "social-media-clone-de887",
+  storageBucket: "social-media-clone-de887.appspot.com",
+  messagingSenderId: "338715349602",
+  appId: "1:338715349602:web:97fca966273665b211373b",
+  measurementId: "G-RZ12LZWF93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <h1 className="nav-header">
+                    My social media site clone
+                </h1>
+                <ul>
+                    <li>Home</li>
+                    <li>My Profile</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
